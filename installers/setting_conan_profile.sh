939735bf52e1a1de152a0d0a76d84e195ff4297d
BRUNO B ZAFFARI <bruno.zaffari@edu.pucrs.br>
2025-09-14 14:31:33 -0300

    Add script to set up conan profile with defaults

---
+++ installers/setting_conan_profile.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+set -e
+
+TAG="[$(basename "${BASH_SOURCE[0]}")]"
+LINE_BRK="\n\n"
+SEGMENT="===========================================================\n"
+
+printf "$SEGMENT"
+printf "$SEGMENT"
+printf "$SEGMENT"
+printf "                   $TAG"
+
+#-----------------------------------------
+printf "$LINE_BRK$SEGMENT"
+printf "$TAG conan profile detect --force\n"
+conan profile detect --force
+printf "$LINE_BRK$SEGMENT"
+#-----------------------------------------
+
+#-----------------------------------------
+printf "$LINE_BRK$SEGMENT"
+printf "$TAG Finding\n"
+
+PROFILE_DIR=$(find . -type d -wholename "*/.conan2/profiles" | head -n 1 || true)
+[ -z "$PROFILE_DIR" ] && PROFILE_DIR="$HOME/.conan2/profiles" && mkdir -p "$PROFILE_DIR"
+
+printf "$TAG Found at $PROFILE_DIR\n"
+printf "$LINE_BRK$SEGMENT"
+#-----------------------------------------
+
+#-----------------------------------------
+printf "$LINE_BRK$SEGMENT"
+printf "$TAG Writing default profile\n"
+
+
+# New Setup (old had compiler.cppstd=17 and compiler.version=11)
+cat << EOF > "$PROFILE_DIR/default"
+[settings]
+arch=x86_64
+build_type=Release
+compiler=gcc
+compiler.cppstd=20
+compiler.libcxx=libstdc++11
+compiler.version=13
+os=Linux
+EOF
+
+printf "$LINE_BRK$SEGMENT"
+#-----------------------------------------
+
+printf "$TAG Profile created in: $PROFILE_DIR/default\n"
+printf "$TAG Checking: cat < $PROFILE_DIR/default $LINE_BRK"
+
+cat < $PROFILE_DIR/default
+printf "$LINE_BRK"
+
+printf "$SEGMENT\n"
+
+printf "\nHard-check with: cat < $PROFILE_DIR/default$LINE_BRK"
+
+printf "$SEGMENT"
+printf "$SEGMENT"
+printf "$SEGMENT"
