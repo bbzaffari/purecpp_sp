02053e0cf45adf0aef025fd02211a025e4fb9c6e
BRUNO B ZAFFARI <bruno.zaffari@edu.pucrs.br>
2025-09-14 16:52:00 -0300

    Update conan profile settings and logging format

--- installers/setting_conan_profile.sh
+++ installers/setting_conan_profile.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 set -e
 
+#-----------------------------------------
+#================= LOGGIN ================
+#-----------------------------------------
 TAG="[$(basename "${BASH_SOURCE[0]}")]"
 LINE_BRK="\n\n"
 SEGMENT="===========================================================\n"
 
-printf "$SEGMENT"
-printf "$SEGMENT"
-printf "$SEGMENT"
-printf "                   $TAG"
+printf "$SEGMENT$SEGMENT$SEGMENT"
+printf "                   $TAG$LINE_BRK"
+#-----------------------------------------
 
 #-----------------------------------------
-printf "$LINE_BRK$SEGMENT"
+printf "$SEGMENT"
 printf "$TAG conan profile detect --force\n"
 conan profile detect --force
-printf "$LINE_BRK$SEGMENT"
 #-----------------------------------------
 
 #-----------------------------------------
@@ -25,23 +26,23 @@ PROFILE_DIR=$(find . -type d -wholename "*/.conan2/profiles" | head -n 1 || true
 [ -z "$PROFILE_DIR" ] && PROFILE_DIR="$HOME/.conan2/profiles" && mkdir -p "$PROFILE_DIR"
 
 printf "$TAG Found at $PROFILE_DIR\n"
-printf "$LINE_BRK$SEGMENT"
-#-----------------------------------------
 
 #-----------------------------------------
+
+#-----------------------------------------
 printf "$LINE_BRK$SEGMENT"
-printf "$TAG Writing default profile\n"
+printf "$TAG Writing default profile$LINE_BRK"
 
 
-# New Setup (old had compiler.cppstd=17 and compiler.version=11)
+# Old Setup (New was set to compiler.cppstd=20 and compiler.version=13. But was resulting in issues.)
 cat << EOF > "$PROFILE_DIR/default"
 [settings]
 arch=x86_64
 build_type=Release
 compiler=gcc
-compiler.cppstd=20
+compiler.cppstd=17
 compiler.libcxx=libstdc++11
-compiler.version=13
+compiler.version=11
 os=Linux
 EOF
 
@@ -58,6 +59,9 @@ printf "$SEGMENT\n"
 
 printf "\nHard-check with: cat < $PROFILE_DIR/default$LINE_BRK"
 
-printf "$SEGMENT"
-printf "$SEGMENT"
-printf "$SEGMENT"
+#-----------------------------------------
+#================= ENDING ================
+#-----------------------------------------
+printf "$SEGMENT$SEGMENT$SEGMENT"
+printf "\n\n\n\n\n".
+#-----------------------------------------
