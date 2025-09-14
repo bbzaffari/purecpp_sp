16d9fa9e1b846b3f1aa3269a2e1de3f79a60077d
BRUNO B ZAFFARI <bruno.zaffari@edu.pucrs.br>
2025-09-14 16:51:30 -0300

    Enhance install_torch.sh with logging and safety
    Added logging functionality and improved error handling.

--- installers/install_torch.sh
+++ installers/install_torch.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
+set -e
 
+#-----------------------------------------
+#================= LOGGIN ================
+#-----------------------------------------
+set -euo pipefail
+
+TAG="[$(basename "${BASH_SOURCE[0]}")]"
+LINE_BRK="\n\n"
+SEGMENT="===========================================================\n"
+
+printf "$SEGMENT$SEGMENT$SEGMENT"
+printf "                   $TAG$LINE_BRK"
+printf "$SEGMENT"
+printf "$LINE_BRK"
+#-----------------------------------------
+
+#-----------------------------------------
 libtorch_cpu_zip=libtorch-cxx11-abi-shared-with-deps-2.5.0+cpu.zip
 
 libtorch_cpu_url=https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-2.5.0%2Bcpu.zip
@@ -19,3 +36,11 @@ wget ${libtorch_cpu_url} -O ${libtorch_cpu_zip}
 unzip ${libtorch_cpu_zip}
 mv ./libtorch/* ../libs/libtorch/cpu 
 rm -fr libtorch/ 
+#-----------------------------------------
+
+#-----------------------------------------
+#================= ENDING ================
+#-----------------------------------------
+printf "$SEGMENT$SEGMENT$SEGMENT"
+printf "\n\n\n\n\n".
+#-----------------------------------------
